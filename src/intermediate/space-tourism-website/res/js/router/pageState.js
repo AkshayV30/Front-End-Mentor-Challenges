bc9550453167079a0a84f1967ad84d5d6bd28c13
AkshayV30 <akshayatwork.v30@gmail.com>
2026-05-09 19:16:44 +0530

    style: finalize desktop layout and responsive CSS for Space Tourism

--- src/intermediate/space-tourism-website/res/js/router/pageState.js
+++
@@ -1,9 +0,0 @@
-export function setPageState(page) {
-  const body = document.body;
-
-  [...body.classList]
-    .filter((cls) => cls.startsWith("page-"))
-    .forEach((cls) => body.classList.remove(cls));
-
-  body.classList.add(`page-${page}`);
-}
--- src/intermediate/space-tourism-website/res/js/store.js
+++
@@ -1,4 +0,0 @@
-export const store = {
-  data: {},
-  currentPage: "home",
-};
